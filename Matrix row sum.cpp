081b0645933ce41a478a51b76b087744af4f31b9
Ramavath Rahul <21311a05f1@sreenidhi.edu.in>
2024-06-11 11:37:33 +0530

    Matrix row sum.cpp

---
+++ Matrix row sum.cpp
@@ -0,0 +1,46 @@
+/*
+Given a matrix of size N x M, print row-wise sum, separated by a newline.
+Note: Try to solve this without declaring/storing the matrix.
+
+Input Format:
+First line of input contains N, M - the size of the matrix. Its followed by N lines each containing M integers - elements of the matrix.
+
+Constraints:
+1 <= N, M <= 100
+-106 <= ar[i] <= 106
+
+Output Format:
+Print row-wise sum of the matrix, separated by a newline.
+
+Sample Input:
+2 3
+5 -1 3
+19 8 -5
+
+Sample Output:
+7
+22
+*/
+
+//Program
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int m,n;
+    cin>>m>>n;
+    if(m<1 || n>100)
+        return 0;
+    for(int i=0;i<m;i++) {
+        int sum=0;
+        for(int j=0;j<n;j++) {
+            int num;
+            cin>>num;
+            sum+=num;
+        }
+        cout << sum << endl;
+    }
+    return 0;
+}
+
+//Space Complexity: O(1)
+//Time Complexity: O(m*n)
