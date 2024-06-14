25d271f8879498404f74cb91cf938959e021763b
Ramavath Rahul <21311a05f1@sreenidhi.edu.in>
2024-06-14 11:47:08 +0530

    Solution in C++

--- Cubes sum.cpp
+++ Cubes sum.cpp
@@ -22,22 +22,22 @@ Sample Output:
 using namespace std;
 int main() {
     int n;
-    cin>>n;
-    int sum=0;
-    for(int i=1;i<=n;i++)
-        sum+=(i*i*i);
-    cout<<sum;                                                  // Space Complexity: O(1)
-    return 0;                                                   // Time Complexity: O(n)
-}
+    cin>>n;                                                          //taking the input
+    int sum=0;                                                      //intially sum will be 0
+    for(int i=1;i<=n;i++)                                           //iterating from 1 to n
+        sum+=(i*i*i);                                               //multiplying each number themselves three times then adding to the sum
+    cout<<sum;                                                      //printing the sum
+    return 0;                                                    
+}                                                                   // Space Complexity: O(1)          // Time Complexity: O(n)
 
 //Optimal approach
 #include<bits/stdc++.h>
 using namespace std;
 int main() {
     int n;
-    cin>>n;
-    int ans=(n*(n+1))/2;
-    int sum=ans*ans;                                       //formula for sum of cubes of first N natural numbers
-    cout<<sum;                                                  // Space Complexity: O(1)
-    return 0;                                                   // Time Complexity: O(1)
-}
+    cin>>n;                                                       //taking the input
+    int ans=(n*(n+1))/2;                                          //since it is the formula for finding sum of first natural numbers
+    int sum=ans*ans;                                             //by sqaring the above formula we get the formula for sum of cubes of first n natural numbers
+    cout<<sum;                                                   //printing the sum
+    return 0;
+}                                                                // Space Complexity: O(1)           // Time Complexity: O(1)
