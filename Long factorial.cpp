dc7da70d901796c90f571c74ba0d1cf39a2bb7e9
Ramavath Rahul <21311a05f1@sreenidhi.edu.in>
2024-06-14 12:25:12 +0530

    4 approaches

--- Long factorial.cpp
+++ Long factorial.cpp
@@ -22,45 +22,45 @@ Sample Output:
 using namespace std;
 int main() {
     int n;
-    cin>>n;
-    long long f=1;
-    for(int i=1;i<=n;i++)
-        f*=i;
-    cout<<f;                                //Space Complexity: O(1)
-    return 0;                               //Time Complexity: O(n)
-}
+    cin>>n;                                     //taking the input
+    long long f=1;                              //initially f value will be 1
+    for(int i=1;i<=n;i++)                       //iterating from 1 to n
+        f*=i;                                   //multiplying f one by one from 1 to n
+    cout<<f;                                    //printing the answer
+    return 0;                               
+}                                               //Space Complexity: O(1)          //Time Complexity: O(n)
 
 
 //Recursive approach
 #include<bits/stdc++.h>
 using namespace std;
 long long fact(int n){
-    if(n<=1) return n;
-    return n*fact(n-1);
+    if(n<=1) return n;                           //returns the factorial of n
+    return n*fact(n-1);                         //recursive call to multipy if still there are numbers to multiply
 }
 int main() {
     int n;
-    cin>>n;
-    cout<<fact(n);                         //Space Complexity: O(n) (Auxiliary Space)
-    return 0;                              //Time Complexity: O(n)
-}
+    cin>>n;                                      //taking the input
+    cout<<fact(n);                               //calling the fact() function then printing the answer
+    return 0;                              
+}                                               //Space Complexity: O(n) (Auxiliary Space)         //Time Complexity: O(n)
 
 
 //Dynamic Programming approach
 #include<bits/stdc++.h>
 using namespace std;
 long long fact(int n) {
-    vector<long long> dp(n+1, 1);            // Initialize DP table with 1's
-    for (int i=2;i<=n;i++)
-        dp[i]=dp[i-1] * i;                   // Use previous factorial result to calculate the current one
-    return dp[n];                            // Return the factorial of n
+    vector<long long> dp(n+1, 1);               //declare and initialize DP table with 1's
+    for (int i=2;i<=n;i++)                      //iterates from 2 to n
+        dp[i]=dp[i-1] * i;                      //use previous factorial result to calculate
+    return dp[n];                               //returns the factorial of n
 }
 int main() {
     int n;
-    cin>>n;
-    cout <<fact(n);                          //Space Complexity: O(n)
-    return 0;                               //Time Complexity: Amortized O(n)
-}
+    cin>>n;                                     //taking the input
+    cout <<fact(n);                            //calling te fact() function
+    return 0;                               
+}                                              //Space Complexity: O(n)        //Time Complexity: Amortized O(n)
 
 
 //Backtracking approach
@@ -68,13 +68,13 @@ int main() {
 using namespace std;
 long long fact(int n,long long result) {
     if(n == 0 || n == 1)
-        return result;
-    return fact(n-1,result * n);
+        return result;                          //returns the factorial of n
+    return fact(n-1,result * n);                //recursive call to multipy if still there are numbers to multiply
 }
 
 int main() {
     int n;
-    cin>>n;
-    cout<<fact(n,1);                         //Space Complexity: O(n)  (Auxiliary space)
-    return 0;                                //Time Complexity: O(n)
-}
+    cin>>n;                                     //taking the input
+    cout<<fact(n,1);                           //calling the fact() function with inital result value as 1
+    return 0;                                
+}                                             //Space Complexity: O(n)  (Auxiliary space)          //Time Complexity: O(n)
