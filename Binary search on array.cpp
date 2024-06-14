5bd2261837d89cfe3654359d20bd3c024757ccd0
Ramavath Rahul <21311a05f1@sreenidhi.edu.in>
2024-06-14 11:29:46 +0530

    2 approaches

--- Binary search on array.cpp
+++ Binary search on array.cpp
@@ -25,56 +25,57 @@ true
 #include<bits/stdc++.h>
 using namespace std;
 bool binarySearch(int arr[],int n,int k){
-    int low=0,high=n-1;
+    int low=0,high=n-1;                                             //initially low points to first element and high points to last element
     while(low<=high){
-        int mid=(low+high)/2;
+        int mid=(low+high)/2;                                       //calculation of mid
         if(arr[mid]==k)
-            return true;
+            return true;                                            //returning true if we find the answer
         else if(arr[mid]<k)
-            low=mid+1;
+            low=mid+1;                                              //eliminating the left part if our anser is not in the left part
         else
-            high=mid-1;
+            high=mid-1;                                             //eliminating the right part if our anser is not in the right part
     }
-    return false;
+    return false;                                                   //returning false if we didn't found our answer after searching entire array
 }
 int main(){
     int n,k;
-    cin>>n>>k;
+    cin>>n>>k;                                                        //taking no. of elements and element to search as inputs
     int arr[n];
     for(int i=0;i<n;i++)
         cin>>arr[i];
-    bool ans=binarySearch(arr,n,k);
+    bool ans=binarySearch(arr,n,k);                                   //calling the binary search function
     if(ans)
-        cout<<"true";                                                 //Space Complexity: O(1)
-    else                                                              //Time Complexity: O(n)
-        cout<<"false";
+        cout<<"true";                                                 //executes if binary search function returns true
+    else                                                              
+        cout<<"false";                                                //executes if binary search function returns false
     return 0;
-}
+}                                                                    //Space Complexity: O(1)       //Time Complexity: O(log n)
 
 //Recursive approach
 #include<bits/stdc++.h>
 using namespace std;
 bool binarySearch(int arr[],int n,int k,int low,int high){
     if(low>high)
-        return false;
+        return false;                                                      //returning false if we didn't found our answer
     int mid=(low+high)/2;
     if(arr[mid]==k)
-        return true;
+        return true;                                                      //returning true if we find the answer
     else if(arr[mid]<k)
-        return binarySearch(arr,n,k,mid+1,high);
+        return binarySearch(arr,n,k,mid+1,high);                          //eliminating the left part if our anser is not in the left part
     else
-        return binarySearch(arr,n,k,low,mid-1);
+        return binarySearch(arr,n,k,low,mid-1);                           //eliminating the right part if our anser is not in the right part
+    }
 }
 int main(){
     int n,k;
-    cin>>n>>k;
+    cin>>n>>k;                                                           //taking no. of elements and element to search as inputs
     int arr[n];
     for(int i=0;i<n;i++)
         cin>>arr[i];
-    bool ans=binarySearch(arr,n,k,0,n-1);
+    bool ans=binarySearch(arr,n,k,0,n-1);                                //calling the binary search function
     if(ans)
-        cout<<"true";                                                  //Space Complexity: O(n) (Auxiliary space)
-    else                                                               //Time Complexity: O(n)
-        cout<<"false";
+        cout<<"true";                                                    //executes if binary search function returns true
+    else                                                               
+        cout<<"false";                                                  //executes if binary search function returns false
     return 0;
-}
+}                                                                       //Space Complexity: O(n) (Auxiliary space)            //Time Complexity: O(log n)
