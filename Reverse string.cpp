0e62f106f457160244b085e3220e314cc6edef50
Ramavath Rahul <21311a05f1@sreenidhi.edu.in>
2024-06-22 21:43:47 +0530

    3 appproaches

--- Reverse string.cpp
+++ Reverse string.cpp
@@ -25,11 +25,21 @@ int main() {
     string str;
     cin>>str;
     int n=str.size();
-    for(int i=n-1;i>=0;i--)                                 // Space Complexity: O(1)
-        cout<<str[i];                                       // Time Complexity: O(n)
-    return 0;
+    for(int i=n-1;i>=0;i--)                                 //traversing from last to first indices of string         
+        cout<<str[i];
+    return 0;                                               //Space Complexity: O(1)        //Time Complexity: O(n)
 }
 
+//Using STL
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin>>str;
+    reverse(str.begin(),str.end());                         //using reverse() function that reverses the entire string
+    cout<<str;
+    return 0;                                               //Space Complexity: O(1)        //Time Complexity: O(n)
+}
 //Optimal approach
 #include<bits/stdc++.h>
 using namespace std;
@@ -38,7 +48,7 @@ int main() {
     cin>>str;
     int n=str.size();
     for(int i=0;i<n/2;i++)
-        swap(str[i],str[n-i-1]);                            // Space Complexity: O(1)
-    cout<<str;                                              // TIme Complexity: O(n/2) ~ O(n)
-    return 0;
+        swap(str[i],str[n-i-1]);                            //swapping first and last characters by incrementing first inex and decrementing last index until we reach middle of the string
+    cout<<str;                                              
+    return 0;                                                //Space Complexity: O(1)          // TIme Complexity: O(n/2) ~ O(n)
 }
